05e1f0b50887d1f820fbed52f5e3d9e1c4ef5910
Melobgn <melodybugnon@gmail.com>
2024-04-08 11:13:28 +0200

    crawling ok

--- melo/jpboxoffice/jpboxoffice/spiders/jpspider.py
+++ melo/jpboxoffice/jpboxoffice/spiders/jpspider.py
@@ -15,6 +15,9 @@ class JpspiderSpider(scrapy.Spider):
 }
     def parse(self, response):
         movies = response.css('td.col_poster_titre')
+        entrees_premiere_semaine = response.css('table.tablesmall.tablesmall5 tr td.col_poster_contenu_majeur::text').get()
+        salles_premiere_semaine = response.css('table.tablesmall.tablesmall5 tr:nth-child(2) td:nth-child(7)::text').get()
+
         for movie in movies:
             relative_url = movie.xpath('//*[@id="content"]//td[3]/h3/a/@href').get()
             # print("Relative URL:", relative_url)
@@ -27,7 +30,7 @@ class JpspiderSpider(scrapy.Spider):
             # Ajoute l'URL à l'ensemble des URL visitées
             else:
                 self.urls_vues.add(movie_url)
-            yield response.follow(movie_url, callback=self.parse_movie_page)
+            yield response.follow(movie_url, callback=self.parse_movie_page, meta={'entrees_premiere_semaine' : entrees_premiere_semaine, 'salles_premiere_semaine' : salles_premiere_semaine})
 
         current_page = response.meta.get('current_page', 0)
         next_page = current_page + 30
@@ -40,6 +43,10 @@ class JpspiderSpider(scrapy.Spider):
     def parse_movie_page(self, response):
         movie_item = JpboxofficeItem()
 
+        entrees_premiere_semaine = response.meta.get('entrees_premiere_semaine')
+        salles_premiere_semaine = response.meta.get('salles_premiere_semaine')
+
+
         movie_item['url'] = response.url
         movie_item['titre'] = response.xpath('//h1/text()').get()
         movie_item['realisateur'] = response.css('table.table_2022titre h4 a::text').get()
@@ -50,10 +57,9 @@ class JpspiderSpider(scrapy.Spider):
         movie_item['casting'] = response.xpath('//div[5]/div[1]/ul/li[6]/a/text()')[1].extract().strip()
         movie_item['franchise'] = response.xpath('//div[@id="nav2"]//ul//a[contains(text(), "Franchise")]/text()').get()
         movie_item['remake'] = response.xpath('//div[@id="nav2"]//ul//a[contains(text(), "Remake")]/text()').get()
-        movie_item['nombre_entrees_premiere_semaine'] = response.xpath("//table[contains(@class, 'tablesmall') and contains(@class, 'tablesmall2')]/tr[9]/td[contains(@class, 'col_poster_contenu_majeur')]/text()").get()
         movie_item['poids_premiere_semaine'] = response.xpath("//table[contains(@class, 'tablesmall') and contains(@class, 'tablesmall2')]/tr[9]/td[3]/text()").get()
-        movie_item['nombre_salles_premiere_semaine'] = response.xpath("//table[contains(@class, 'tablesmall') and contains(@class, 'tablesmall5')]/tr[3]/td[6]/text()").get()
-        
+        movie_item['entrees_premiere_semaine'] = entrees_premiere_semaine
+        movie_item['salles_premiere_semaine'] = salles_premiere_semaine        
         
         li5_text = response.xpath('//*[@id="nav2"]/ul/li[5]/a/text()')[-1].extract()
         li6_text = response.xpath('//*[@id="nav2"]/ul/li[6]/a/text()')[-1].extract()
@@ -69,6 +75,10 @@ class JpspiderSpider(scrapy.Spider):
             yield response.follow(casting_url, callback=self.parse_casting, meta={'movie_item': movie_item})
 
 
+        budget_url = response.xpath('//*[@id="nav2"]/ul/li[1]/a/@href').get()
+        yield response.follow(budget_url, callback=self.parse_budget, meta={'movie_item' : movie_item})
+
+
 
     def parse_casting(self, response):
         #'response.meta' pour accéder aux métadonnées transmises
@@ -77,4 +87,9 @@ class JpspiderSpider(scrapy.Spider):
         movie_item['producteur'] = response.xpath('//tr/td[contains(@class, "col_poster_titre") and @itemprop="producer"]/h3/a[@itemprop="name"]/text()').get()
         movie_item['compositeur'] = response.xpath('//tr/td[contains(@class, "col_poster_titre") and @itemprop="compositor"]/h3/a[@itemprop="name"]/text()').get()
 
+
+    def parse_budget(self, response):
+        movie_item = response.meta['movie_item']
+        movie_item['budget'] = response.css('table.tablesmall.tablesmall1b tr td div strong::text').get()
+        
         yield movie_item
