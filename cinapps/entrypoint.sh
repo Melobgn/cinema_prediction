d06c92aa9c6aa5bc670d76ea146a6d69ffd2a7c3
Melobgn <melodybugnon@gmail.com>
2024-04-17 15:45:43 +0200

    dockerfile ok

--- cinapps/entrypoint.sh
+++ cinapps/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "En attente de la disponibilité de Postgres..."
-  sleep 1
-done
+# while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+#   echo "En attente de la disponibilité de Postgres..."
+#   sleep 1
+# done
 
 # Effectuer les migrations
 echo "Effectuer les migrations de la base de données..."
@@ -16,8 +16,8 @@ echo "Lancer le serveur"
 
 ### production ###
 # ### Version avec gunicorn pour le serveur web
-# gunicorn cine_app.wsgi:application --workers=4 --bind=0.0.0.0:8001 --reload
+gunicorn cinapps.wsgi:application --workers=4 --bind=0.0.0.0:8000 --reload
 
 
 # dev en local ###
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
